45cadc86fcf7ba8e7f1a09d94344a8417557104f
ibrartt <111246165+ibrar19911@users.noreply.github.com>
2023-05-08 16:06:23 +0500

    dep.php with password

--- dep.php
+++ dep.php
@@ -1,22 +1,12 @@
 <?php
 
+$p = $_GET['p'];
 
-$secret = '!C91@Ui*w&9F6'; // Replace with your secret key
+$pe = md5($p);
 
-$headers = getallheaders();
-$signature = isset($headers['X-Hub-Signature']) ? $headers['X-Hub-Signature'] : '';
-$body = file_get_contents('php://input');
-
-if (!verify_signature($secret, $signature, $body)) {
-    http_response_code(401);
-    exit('Unauthorized');
+if( $pe == 'be0817be680150598adf711ebd7d917c' ){
+	exec('git pull');
+}else{
+	die('not authorised');
 }
 
-// Pull the changes from GitHub
-exec('git pull');
-
-function verify_signature($secret, $signature, $payload) {
-    list($algo, $hash) = explode('=', $signature, 2);
-    $payload_hash = hash_hmac($algo, $payload, $secret);
-    return hash_equals($hash, $payload_hash);
-}
